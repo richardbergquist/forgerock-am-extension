dba5287e18e88b08921b46c980e4a0ade5240029
richardbergquist <richardbe@datacom.co.nz>
2018-03-07 15:12:48 +1300

    An AM HOTP SMS custom gateway framework.

---
+++ am-hotp/src/main/java/nz/co/bergquistr/forgerock/am/CustomSMSGateway.java
@@ -0,0 +1,61 @@
+package nz.co.bergquistr.forgerock.am;
+
+
+//import com.sun.identity.authentication.modules.hotp.DefaultSMSGatewayImpl;
+import com.sun.identity.authentication.spi.AuthLoginException;
+import com.sun.identity.authentication.modules.hotp.SMSGateway;
+import com.sun.identity.shared.debug.Debug;
+
+import java.util.Map;
+
+public class CustomSMSGateway implements SMSGateway {
+
+  protected Debug debug = null;
+
+  public CustomSMSGateway() {
+    this.debug = Debug.getInstance("amAuthHOTP");
+  }
+
+  /**
+   * Sends a SMS message to the phone with the code
+   * <p>
+   *
+   * @param from The address that sends the SMS message
+   * @param to The address that the SMS message is sent
+   * @param subject The SMS subject
+   * @param message The content contained in the SMS message
+   * @param code The code in the SMS message
+   * @param options The SMS gateway options defined in the HOTP authentication
+   * module
+   * @throws AuthLoginException In case the module was unable to send the SMS
+   */
+  public void sendSMSMessage(String from, String to, String subject, String message, String code, Map options) throws AuthLoginException {
+
+    if (to == null) {
+      return;
+    }
+
+    if (debug.messageEnabled()) {
+      debug.message("CustomSMSGateway.sendSMSMessage() : " +  " HOTP sending to : " + to + ".");
+    }
+  }
+
+  /**
+   * Sends an email  message to the mail with the code
+   * <p>
+   *
+   * @param from The address that sends the E-mail message
+   * @param to The address that the E-mail message is sent
+   * @param subject The E-mail subject
+   * @param message The content contained in the E-mail message
+   * @param code The code in the E-mail message
+   * @param options The SMS gateway options defined in the HOTP authentication
+   * module
+   * @throws AuthLoginException In case the module was unable to send the e-mail
+   */
+  public void sendEmail(String from, String to, String subject, String message, String code, Map options) throws AuthLoginException {
+
+  }
+
+
+}
